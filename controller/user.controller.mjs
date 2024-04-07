b8cca97a976f8c303eb0e497c82d4792e3a6320a
seraj-b2 <125463271+seraj-b2@users.noreply.github.com>
2024-04-07 07:39:47 +0530

    passkey check add

--- controller/user.controller.mjs
+++ controller/user.controller.mjs
@@ -9,12 +9,16 @@ const login = (req, res) => {
 
     const date = formatDate(new Date());
     const passkeyShouldBe = CryptoJS.MD5(date).toString();
+    if (passkey !== passkeyShouldBe) {
+      console.log("user login failed");
+      return res.send({ success: false });
+    }
 
     const jwt_secret_key = process.env.UUID_NAMESPACE;
     const token = jwt.sign({}, jwt_secret_key, { expiresIn: "1h" });
     console.log("token generated", date, passkey, passkeyShouldBe, jwt_secret_key, token);
 
-    res.cookie('codesnip', token, { httpOnly: true, secure: true, });
+    res.cookie("codesnip", token, { httpOnly: true, secure: true });
     res.send({ success: true, token: token });
   } catch (e) {
     console.error(chalk.red("Error occured in login controller"), e);
